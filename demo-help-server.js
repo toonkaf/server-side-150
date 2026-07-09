a3c9c43e66e3d2ee2576e5b6eb3ca161cf833389
Ratchanon Yaemboo <67070150@kmitl.ac.th>
2026-07-09 13:28:11 +0700

    Add /about and /contact routes

--- demo-help-server.js
+++ demo-help-server.js
@@ -4,7 +4,16 @@ const port = 3000;
 const server = http.createServer((req , res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type' , 'text/plain');
-    res.end('Hello World\n');
+    
+    if (req.url === '/about') {
+        res.end('Account Page\n');
+    }
+    else if (req.url === '/contact') {
+        res.end('Contact Page\n');
+    }
+    else {
+        res.end('Hello World\n');
+    }
 });
 
 server.listen(port , hostname, () => {
