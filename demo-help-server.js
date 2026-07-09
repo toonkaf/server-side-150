75e5a0dae08654431e8d7a555e45550cf23f49ed
Ratchanon Yaemboo <67070150@kmitl.ac.th>
2026-07-09 13:50:53 +0700

    add /api route with JSON response

--- demo-help-server.js
+++ demo-help-server.js
@@ -2,16 +2,26 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 const server = http.createServer((req , res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type' , 'text/plain');
-    
     if (req.url === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type' , 'text/plain');
         res.end('Account Page\n');
     }
     else if (req.url === '/contact') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type' , 'text/plain');
         res.end('Contact Page\n');
     }
+    else if (req.url === '/api'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type' , 'text/plain');
+        const data = {
+            message: 'Hello from API',
+            timestamp: new Date().toISOString()
+        };
+    }
     else {
+        res.statusCode = 200;
         res.end('Hello World\n');
     }
 });
